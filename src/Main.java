11690f5c1cbaeb920fb66394fc58f543bb28b304
SokolovAndr <korch45@yandex.ru>
2023-03-20 22:41:18 +0300

    Добавили полиморфизм через @Override

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //ООП: Классы и объекты
-        Car bmw = new Car(300.5f, 2200, "blue", new byte[]{100,100,100});
+//        Car bmw = new Car(300.5f, 2200, "blue", new byte[]{100,100,100});
         Truck truck = new Truck(4990, new byte[]{125,125,125}, false);
-
-        truck.setLoaded(true);
-        truck.getLoaded();
+        truck.setValues(300.5f, 2200, "blue", new byte[]{100,100,100});
+        System.out.println(truck.getValues());
 
     }
 }
--- src/Transport.java
+++ src/Transport.java
@@ -11,22 +11,22 @@ public class Transport {
     public Transport(float _speed, int _weight, String _color, byte[] _coordinate){
         System.out.println("Object created");
         setValues(_speed,_weight,_color,_coordinate);
-        System.out.println(getValues());
+        //System.out.println(getValues());
     }
     public Transport(int weight, byte[] coordinate){
         System.out.println("Object created");
         this.weight = weight;
         this.coordinate = coordinate;
-        System.out.println(getValues());
+        //System.out.println(getValues());
     }
-    public void setValues(float _speed, int _weight, String _color, byte[] _coordinate){
-        speed = _speed;
-        weight = _weight;
-        color = _color;
-        coordinate = _coordinate;
+    protected void setValues(float speed, int weight, String color, byte[] coordinate){
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
     }
 
-    public String getValues(){
+    protected String getValues(){
         String info = "Object speed: " + speed + ", Weight: "+ weight+", Color: " +color + "\n";
         String infoCoordinates = "Coordinates: ";
         for (int i=0; i<coordinate.length;i++)
